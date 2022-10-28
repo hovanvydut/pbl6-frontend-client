f8b52f4d5eb5a03cf8204772285591e8159adcb5
Phuong Tran <tranphiphuong2763@gmail.com>
2022-10-29 01:14:57 +0700

    [Filter] update UI filter and handle clear filter

--- src/app/modules/filter/filter.component.ts
+++ src/app/modules/filter/filter.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonService } from '@app/core/services/common.service';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription, Subject } from 'rxjs';
+//
+import { ENDPOINTS } from '@app/shared/utilities';
 import { ItemModel } from '@app/shared/models/base.model';
 import { PropertiesModel } from '@app/shared/models/property.model';
-import { PropertyEnum } from '../post/enums/property.enum';
 import { QueryParams } from '../post/models/post.model';
-import { FormControl } from '@angular/forms';
-import { Subscription, Subject } from 'rxjs';
+import { CommonService } from '@app/core/services/common.service';
 import { FilterService } from './filter.service';
-import { Router, RouterStateSnapshot } from '@angular/router';
-import { ENDPOINTS } from '@app/shared/utilities';
 
 @Component({
   selector: 'app-filter',
@@ -36,7 +36,7 @@ export class FilterComponent implements OnInit {
   } = {
     maxArea: new FormControl(100),
     minArea: new FormControl(0),
-    maxPrice: new FormControl(1000000),
+    maxPrice: new FormControl(30000000),
     minPrice: new FormControl(0),
     searchValue: new FormControl('')
   };
@@ -47,7 +47,7 @@ export class FilterComponent implements OnInit {
   constructor(
     private commonService: CommonService,
     private filterService: FilterService,
-    private router: Router,
+    private router: Router
   ) {
     this.subscription.add(
       this.filterService._queryParams.subscribe(params => {
@@ -86,9 +86,7 @@ export class FilterComponent implements OnInit {
   }
 
   onSelectedFieldChanged(params) {
-    // check if the property is already selected
     if (params.value) {
-      //  update properties value
       this.properties.findIndex(property => {
         if (property.id === params.type) {
           property.value = params.value;
@@ -102,13 +100,26 @@ export class FilterComponent implements OnInit {
     this.onValueChanged();
   }
 
+  onClearFilterButtonClicked () {
+    this.properties.forEach(property => {
+      property.value = [];
+    });
+    this.filterParams = {
+      maxArea: new FormControl(100),
+      minArea: new FormControl(0),
+      maxPrice: new FormControl(30000000),
+      minPrice: new FormControl(0),
+      searchValue: new FormControl('')
+    }
+    this.filterService.setQueryParams(this.queryParams);
+  }
+
   onFilterButtonClicked() {
     this.queryParams.maxArea = this.filterParams.maxArea.value;
     this.queryParams.minArea = this.filterParams.minArea.value;
     this.queryParams.maxPrice = this.filterParams.maxPrice.value;
     this.queryParams.minPrice = this.filterParams.minPrice.value;
     this.queryParams.properties = [];
-    console.log(this.properties)
     this.properties.forEach(property => {
       if (property.value.length > 0) {
         this.queryParams.properties.push(property.id as string);
@@ -118,18 +129,25 @@ export class FilterComponent implements OnInit {
     this.onValueChanged();
   }
 
-  formatLabel(value: number) {
-    if (value >= 1000000) {
-      return Math.round(value / 1000000) + 'tr';
-    }
-
-    return value;
-  }
-
   onValueChanged() {
     this.filterParams$.next();
     if (this.router.url !== ENDPOINTS.POSTS_FILTER) {
       this.router.navigateByUrl(ENDPOINTS.POSTS_FILTER).then();
+    }
+  }
+
+  handleSliderChanged(value: { max: number; min: number }, type: string) {
+    switch (type) {
+      case 'area':
+        this.filterParams.maxArea.setValue(value.max);
+        this.filterParams.minArea.setValue(value.min)
+        break;
+      case 'price':
+        this.filterParams.maxPrice.setValue(value.max)
+        this.filterParams.minPrice.setValue(value.min)
+        break;
+      default:
+        break;
     }
   }
 }
