89f02266457b64ae597fa6e90a4c0cd31b7287e4
Phuong Tran <tranphiphuong2763@gmail.com>
2022-11-02 02:01:42 +0700

    [Layout] add footer

--- src/app/modules/layout/components/footer/footer.component.ts
+++ src/app/modules/layout/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ENDPOINTS } from '@app/shared/utilities';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  ENDPOINTS = ENDPOINTS;
   constructor() { }
 
   ngOnInit() {
