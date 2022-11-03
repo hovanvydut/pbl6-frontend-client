36e14cbeb43e0edb406dfeab17a455bcaedc1f1f
Phuong Tran <tranphiphuong2763@gmail.com>
2022-11-03 09:59:31 +0700

    [Post] update post detail

---
+++ src/app/shared/pipes/from-now.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+@Pipe({
+  name: 'fromNow'
+})
+export class FromNowPipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    return  moment( new Date(value)).startOf('hour').fromNow();
+  }
+}
