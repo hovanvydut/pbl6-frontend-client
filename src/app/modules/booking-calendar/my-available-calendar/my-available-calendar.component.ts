256958f41f32de0c79a2b063841c6f56229d7aa6
Phuong Tran <tranphiphuong2763@gmail.com>
2022-11-07 17:17:13 +0700

    [Calendar] edit free times

--- src/app/modules/booking-calendar/my-available-calendar/my-available-calendar.component.ts
+++ src/app/modules/booking-calendar/my-available-calendar/my-available-calendar.component.ts
@@ -4,6 +4,7 @@ import { BaseService } from '@app/core/services/base.service';
 import { CalendarTemplateComponent } from '@app/shared/components/form/calendar/calendar-template/calendar-template.component';
 import { DecimalPipe } from '@angular/common';
 import { BookingService } from '../services/booking.service';
+import { TIME_SLOTS, WEEK_DAYS } from '@app/shared/app.constants';
 
 @Component({
   selector: 'app-my-available-calendar',
@@ -13,6 +14,18 @@ import { BookingService } from '../services/booking.service';
 export class MyAvailableCalendarComponent implements OnInit {
   @ViewChild('calendar') calendar: CalendarTemplateComponent;
   freeTimes: any[] = [];
+
+  WEEK_DAYS = [
+    {
+      label: 'Ngay',
+      value: null
+    },
+    ...WEEK_DAYS
+  ];
+  TIME_SLOTS = TIME_SLOTS;
+
+  days = [];
+
   constructor(
     private dialog: MatDialog,
     private decimalPipe: DecimalPipe,
@@ -22,6 +35,37 @@ export class MyAvailableCalendarComponent implements OnInit {
 
   ngOnInit(): void {
     this.initData();
+    this.WEEK_DAYS.forEach((day, index) => {
+      this.days.push({
+        label: day.label,
+        value: day.value,
+        timeSlots: this.TIME_SLOTS.map(time => {
+          return {
+            label: time.label,
+            start: time.start,
+            end: time.end,
+            selected: false
+          };
+        })
+      });
+    });
+
+    console.log(this.days);
+  }
+
+  convertFreeTimes() {
+    this.days.forEach((day, index) => {
+      day.timeSlots.forEach(time => {
+        if(this.freeTimes.find(e => e.day === day.value && e.start === time.start && e.end === time.end)) {
+          time.selected = true;
+        }
+      });
+    });
+  }
+
+  selectTimeSlot(dayIndex, timeIndex) {
+    this.days[dayIndex].timeSlots[timeIndex].selected = !this.days[dayIndex]
+      .timeSlots[timeIndex].selected;
   }
 
   initData() {
@@ -35,20 +79,34 @@ export class MyAvailableCalendarComponent implements OnInit {
             end: parseInt(e.end, 10)
           };
         });
+        this.convertFreeTimes();
       });
   }
 
   onSaveCalendarSettingsButtonClicked() {
-    const data = {
-      data: this.calendar.events.map(e => {
-        return {
-          day: e.start.getDay(),
-          start: e.start.getHours().toString(),
-          end: e.end.getHours().toString()
-        };
-      })
-    };
-    this.bookingService.updateHostFreeTime(data).subscribe(res => {
+    const days = this.days
+      .filter(day => day.value)
+      .map(day => {
+        return day.timeSlots
+          .filter(time => time.selected)
+          .map(time => {
+            return {
+              day: day.value,
+              start: time.start.toString(),
+              end: time.end.toString()
+            };
+          });
+      });
+    this.freeTimes = [];
+    days.forEach(day => {
+      day.forEach(time => {
+        this.freeTimes.push(time);
+      });
+    });
+
+    this.bookingService.updateHostFreeTime({
+      "data": this.freeTimes
+    }).subscribe(res => {
       this.dialog.closeAll();
     });
   }
