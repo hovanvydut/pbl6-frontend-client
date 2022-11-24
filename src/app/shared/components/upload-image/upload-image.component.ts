bc0158578a9255337480bea79b725b305df6d7d9
Trần Thị Phượng <61351523+pphuongdut@users.noreply.github.com>
2022-11-24 18:10:20 +0700

    update ui (#58)

--- src/app/shared/components/upload-image/upload-image.component.ts
+++ src/app/shared/components/upload-image/upload-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { CommonService } from '@app/core/services/common.service';
+import { NotifyService } from '@app/shared/services/notify.service';
 
 @Component({
   selector: 'app-upload-image',
@@ -8,7 +9,7 @@ import { CommonService } from '@app/core/services/common.service';
 })
 export class UploadImageComponent implements OnInit {
   @Output() onImageUploaded: EventEmitter<string> = new EventEmitter();
-  constructor(private commonService: CommonService) { }
+  constructor(private commonService: CommonService, private notifyService: NotifyService ) { }
 
   ngOnInit(): void {
   }
@@ -17,6 +18,10 @@ export class UploadImageComponent implements OnInit {
     const selectedFiles = e.target.files;
     if (selectedFiles && selectedFiles[0]) {
       for (let i = 0; i < selectedFiles.length; i++) {
+        if (selectedFiles[i].size > 2097152) {
+          this.notifyService.notify('File size must be smaller than 2 MB');
+          return;
+        }
         this.commonService.uploadImage(selectedFiles[i]).subscribe(res => {
           this.onImageUploaded.emit(res);
         });
