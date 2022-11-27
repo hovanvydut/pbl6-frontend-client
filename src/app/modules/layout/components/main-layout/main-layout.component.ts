4f261d90fff21bd049b9a6b3044579e6be91d668
Phuong Tran <tranphiphuong2763@gmail.com>
2022-11-27 13:20:42 +0700

    [Notification] add socket

--- src/app/modules/layout/components/main-layout/main-layout.component.ts
+++ src/app/modules/layout/components/main-layout/main-layout.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseService } from '@app/core/services/base.service';
+import { WebSocketService } from '@app/core/services/web-socket.service';
 import { AccountModel } from '@app/modules/auth/models/auth.model';
+import { NotificationResponseModel } from '@app/shared/models/notification.model';
 import { ENDPOINTS } from '@app/shared/utilities';
+import { Subscription } from 'rxjs';
 import { menuItems } from '../../const/menu.const';
 
 @Component({
@@ -17,15 +20,25 @@ export class MainLayoutComponent implements OnInit {
 
   isNotificationVisible: boolean = false;
 
-  constructor(private baseService: BaseService) {
+  private _subscriptions: Subscription = new Subscription();
+
+  
+  constructor(private baseService: BaseService, private webSocketService: WebSocketService) {
     this.accountInfo = this.baseService.currentUser;
   }
 
   ngOnInit() {
+    this._subscribeNotificationBroadCastEvent();
   }
 
   toggleNotification() {
     this.isNotificationVisible = !this.isNotificationVisible;
   }
+
+  private _subscribeNotificationBroadCastEvent() {
+    this._subscriptions.add(this.webSocketService.subscribeNotification().subscribe((res: NotificationResponseModel) => {
+      console.log(res);
+    }));
+}
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://node-2.silk-cat.software/api',
-  webSocketEndpoint: 'wss://',
+  baseUrl: 'https://node-2.silk-cat.software/api',  
+  webSocketEndpoint: 'wss://abc.com/api/websocket',
   googleClientId: '',
 };
 
